0cddb4ffb1191299b43757062d8ddd0fe3630511
Mehin-Rahimli <mehincr-bp217@code.edu.az>
2024-11-17 11:45:38 -0800

    Initial commit

---
+++ ADO.NETTask/Program.cs
@@ -0,0 +1,42 @@
+﻿using ADO.NETTask.Data;
+using ADO.NETTask.Models;
+using ADO.NETTask.Services;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ADO.NETTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            StudentService studentService = new StudentService();
+            Student student=new Student { Name="Anya",Surname="Taylor Joy",Age=28};
+
+            studentService.Add(student);
+
+            studentService.Delete(4);
+
+            Student studentGet=studentService.Get(5);
+            if (studentGet != null)
+            {
+                Console.WriteLine($"Here is student: {studentGet.Id}. {studentGet.Name} {studentGet.Surname} {studentGet.Age}");
+            }
+            else
+            {
+                Console.WriteLine("Student not found");
+            }
+
+            Student updatestudent = new Student { Id = 2, Name = "Tom", Surname = "Hanks", Age = 26 };
+            studentService.Update(updatestudent);
+
+            List<Student> students = studentService.GetAll();
+            foreach (var item in students)
+            {
+                Console.WriteLine(item.Id + " " + item.Name + " " + item.Surname + " " + item.Age);
+            }
+
+        }
+    }
+}
---
+++ ADO.NETTask/Services/StudentService.cs
@@ -0,0 +1,89 @@
+﻿using ADO.NETTask.Data;
+using ADO.NETTask.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADO.NETTask.Services
+{
+    internal class StudentService
+    {
+        private static Sql _sql;
+        public StudentService()
+        {
+
+            _sql = new Sql();
+        }
+
+        public void Add(Student student)
+        {
+            int result = _sql.ExecuteCommand($"INSERT INTO Students VALUES('{student.Name}','{student.Surname}',{student.Age})");
+            if (result > 0)
+            {
+                Console.WriteLine("Commands successfully completed");
+            }
+            else
+            {
+                Console.WriteLine("Error occured");
+            }
+        }
+        public List<Student> GetAll()
+        {
+            List<Student> students = new List<Student>();
+            DataTable table = _sql.ExecuteQuery("SELECT*FROM Students");
+            foreach (DataRow item in table.Rows)
+            {
+                Student student = new Student
+                {
+                    Id = (int)item["Id"],
+                    Name = item["Name"].ToString(),
+                    Surname = item["Surname"].ToString(),
+                    Age = (int)item["Age"]
+                };
+                students.Add(student);
+            }
+            return students;
+        }
+
+        public void Delete(int Id)
+        {
+            _sql.ExecuteCommand($"DELETE FROM Students WHERE Id={Id}");
+        }
+
+        public Student Get(int Id)
+        {
+            DataTable table = _sql.ExecuteQuery($"SELECT* FROM Students WHERE Id={Id}");
+            if (table.Rows.Count > 0)
+            {
+                DataRow item = table.Rows[0];
+
+                Student student= new Student
+                {
+                    Id = (int)item["Id"],
+                    Name = item["Name"].ToString(),
+                    Surname = item["Surname"].ToString(),
+                    Age = (int)item["Age"]
+                };
+                return student;
+            }
+            return null;
+
+        }
+
+        public void Update(Student updatestudent)
+        {  
+            int result= _sql.ExecuteCommand($"UPDATE Students SET Name='{updatestudent.Name}',Surname='{updatestudent.Surname}',Age={updatestudent.Age} WHERE Id={updatestudent.Id}");
+            if (result>0)
+            {
+                Console.WriteLine("Updated successfully");
+            }
+            else
+            {
+                Console.WriteLine("Could not be found");
+            }
+        }
+    }
+}
